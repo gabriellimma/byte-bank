3c82a0c05786f7cd86a61a042af98d576912b5d3
Gabriel Lima <nickspoot@gmail.com>
2022-06-16 00:12:20 -0300

    Revert "creating new methods sacar, depositar. refactoring transferir, isCont…"

--- src/main/kotlin/ContaCorrente.kt
+++ src/main/kotlin/ContaCorrente.kt
@@ -5,17 +5,17 @@ class ContaCorrente {
     var saldo = 0.0
     var extrato = ""
 
-    fun transferir(contaDestino: ContaCorrente, valorTransferencia: Double) {
+    fun transferir(contaOrigem: ContaCorrente, contaDestino: ContaCorrente, valorTransferencia: Double) {
         if (valorTransferencia > 0.00) {
-            if (isContaCapazTransferir(valorTransferencia)) {
-                saldo -= valorTransferencia
+            if (isContaCapazTransferir(contaOrigem, valorTransferencia)) {
+                contaOrigem.saldo -= valorTransferencia
                 contaDestino.saldo += valorTransferencia
-                val saldoAnterior = saldo + valorTransferencia
-                extrato =
-                    "Transferencia realizada \nconta origem: ${numeroConta} para conta: ${contaDestino.numeroConta}\nvalor: $valorTransferencia\nSaldo anterior R\$$saldoAnterior\nSaldo atual: R\$${saldo}"
+                val saldoAnterior = contaOrigem.saldo + valorTransferencia
+                contaOrigem.extrato =
+                    "Transferencia realizada \nconta origem: ${contaOrigem.numeroConta} para conta: ${contaDestino.numeroConta}\nvalor: $valorTransferencia\nSaldo anterior R\$$saldoAnterior\nSaldo atual: R\$${contaOrigem.saldo}"
                 contaDestino.extrato =
-                    "Transferencia recebida \nconta origem: ${numeroConta} para conta: ${contaDestino.numeroConta}\nvalor: $valorTransferencia\nSaldo anterior R\$${contaDestino.saldo - valorTransferencia}\nSaldo atual: R\$${contaDestino.saldo}"
-                println(extrato)
+                    "Transferencia recebida \nconta origem: ${contaOrigem.numeroConta} para conta: ${contaDestino.numeroConta}\nvalor: $valorTransferencia\nSaldo anterior R\$${contaDestino.saldo - valorTransferencia}\nSaldo atual: R\$${contaDestino.saldo}"
+                println(contaOrigem.extrato)
             } else {
                 error("Sua conta nao tem saldo suficiente para esta operacao")
             }
@@ -32,28 +32,18 @@ class ContaCorrente {
         }
     }
 
-    fun isContaPositiva(): Boolean {
-        return saldo > 0.00
+    fun isContaPositiva(contaOrigem: ContaCorrente): Boolean {
+        return contaOrigem.saldo > 0.00
     }
 
-    fun pagarConta(valorPagamento: Double, contaDestino: ContaCorrente) {
-        if (isContaPositiva()) {
-            saldo -= valorPagamento
+    fun pagarConta(valorPagamento: Double, contaOrigem: ContaCorrente, contaDestino: ContaCorrente) {
+        if (isContaPositiva(contaOrigem)) {
+            contaOrigem.saldo -= valorPagamento
             contaDestino.saldo += valorPagamento
         }
     }
 
-    fun isContaCapazTransferir(valorTransferencia: Double): Boolean {
-        return saldo >= valorTransferencia
-    }
-
-    fun depositar(contaDestino: ContaCorrente, valorDeposito: Double) {
-        return if(valorDeposito > 0.00) contaDestino.saldo += valorDeposito
-        else error("valor depositado incorreto")
-    }
-
-    fun sacar(valorSaque: Double){
-        return if (isContaCapazTransferir(valorSaque)) saldo -= valorSaque
-        else error("voce nao tem saldo suficiente para esta operacao.")
+    fun isContaCapazTransferir(contaOrigem: ContaCorrente, valorTransferencia: Double): Boolean {
+        return contaOrigem.saldo >= valorTransferencia
     }
 }
